4cf75fef5de8f8df85efb1102a73231cfb8c677e
Angelo Carzis <acarzis@gmail.com>
2025-01-07 19:17:22 -0500

    Add project files.

---
+++ Metaprogramming/Metaprogramming.cpp
@@ -0,0 +1,36 @@
+﻿#include "Metaprogramming.h"
+#include <vector>
+
+using namespace std;
+
+using int_t = int;																		// this is equivalent to typedef
+template<typename X, typename Y> using pair_vector = std::vector<std::pair<X, Y>>;		// this is a template alias
+
+// this is a variadic template. Args is called a parameter pack. It expands to a comma separated list.
+template<typename... Args> struct variadic_example {};
+
+
+int main()
+{
+	// constexpr tells the compiler that the function can be executed by the compiler and the result can be used at compile time.
+	constexpr power<2, 8> result;		
+	cout << result.value << endl;
+
+	// constexpr double result2 = pow(2,8);		--> this is not valid because pow is not a constexpr function
+
+	// using the template alias
+	pair_vector<int, string> pv;
+	std::pair temp = make_pair(1, "angelo");
+	pv.push_back(temp);
+
+	// using our tuple template class
+	::tuple<bool, int> tuple1(false, 3);
+	printf("tuple1.value = %s\r\n", tuple1.value ? "true" : "false");
+	printf("tuple1.next.value = %d\r\n", tuple1.next.value);
+
+	// tuple example #2
+	::tuple<float,char,const char*> tuple2(3.14, '+', "foo");
+	printf("tuple2.next.next.value = %s\r\n", tuple2.next.next.value);
+
+	return 0;
+}
---
+++ Metaprogramming/Metaprogramming.h
@@ -0,0 +1,76 @@
+﻿#pragma once
+
+#include <iostream>
+
+// analog to pow() at compile time
+template<int X, int Y> struct power {
+    constexpr static const long long value =
+        X * power<X, Y - 1>::value;
+};
+
+// terminating specialization
+template<int X> struct power<X, 0> {
+    constexpr static const long long value = 1;
+};
+
+
+
+// a tuple definition
+//    LIMITATION: the below implementation does not support strings. the original code, as taken from the link below, does (as well as index support)  
+//                reference:  https://www.codeproject.com/Articles/5297814/Metaprogramming-in-Cplusplus-A-Gentle-Introduction - author:  honey the codewitch  
+// 
+// the following takes zero or
+// more arguments
+template <
+    // declare a template parameter
+    // pack:
+    typename... Types>
+struct tuple
+{
+    // our base tuple has no fields and is
+    // therefore empty
+};
+
+
+
+// Declare a specialization for the tuple that
+// takes at least one argument. Because this
+// specialization exists, it will be chosen
+// by the C++ compiler instead of the empty
+// tuple struct template because it's more
+// specific. The compiler will only fall
+// back to the empty definition if there
+// are no arguments, since below will
+// only match one or more of them:
+template <
+    // this "peels" the first argument
+    // from the parameter pack for
+    // processing here:
+    typename Type,
+    // declare a template parameter
+    // pack:
+    typename... Types>
+struct tuple<Type, Types...>
+{
+    // our current tuple value
+    Type value;
+    // here we instantiate a new tuple
+    // with one fewer argument - the first
+    // type in the list that was passed to
+    // us is gone from Types... - it was
+    // peeled off into Type.
+    tuple<Types...> next;
+    // we expand the parameter pack into
+    // arguments to our ctor. Note that
+    // we passed in Type as the first
+    // one. This is because Types...
+    // doesn't contain it anymore
+    // since it was peeled off
+    constexpr inline tuple(
+        const Type& value,
+        const Types &...next)
+        : value(value), next(next...)
+    {
+    }
+};
+
